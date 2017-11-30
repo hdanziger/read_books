2ce18835539945afce3b337ff42663c108fb51c6
Hannah Danziger <hdanziger8@gmail.com>
2017-11-30 20:17:01 +0000

    get to post

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,11 @@ class BooksController < ApplicationController
   get '/books/new' do
     erb :'/books/new'
   end
+
+  post '/books' do
+    @book = Book.create(:title => params[:title])
+    @book.author = Author.find_or_create_by(:name => params[:name])
+    @book.save
+    redirect '/books'
+  end
 end
