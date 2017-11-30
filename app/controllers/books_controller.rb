610980120a21228b09051c9146ef9e9d739a6678
Hannah Danziger <hdanziger8@gmail.com>
2017-11-30 19:54:31 +0000

    new.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+
+  get '/books' do
+    @books = Book.all
+    erb :'/books/index'
+  end
+
+  get '/books/new' do
+    erb :'/books/new'
+  end
 end
