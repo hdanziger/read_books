36c80b7ba19766f3f33e4692a8c41167a56bc7e1
Hannah Danziger <hdanziger8@gmail.com>
2017-12-01 17:12:26 +0000

    authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,14 @@
 class AuthorsController < ApplicationController
+
+  get '/authors' do
+   @authors = Author.all
+   erb :'authors/index'
+ end
+
+ get '/authors/:id' do
+  @author = Author.find_by_id(params[:id])
+  erb :'authors/show'
+end
+
+
 end
